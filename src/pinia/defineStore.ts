fb9f3415789e08253ccea129d41a8c01046a78c3
qqq000qqq@aliyun.com <xqq1258460261>
2022-07-26 15:11:03 +0800

    20220726

--- src/pinia/defineStore.ts
+++ src/pinia/defineStore.ts
@@ -1,14 +1,16 @@
-import { effectScope, getCurrentInstance, inject, reactive } from "vue"
+import { computed, effectScope, getCurrentInstance, inject, reactive, toRefs } from "vue"
 import { SymbolPinia } from "./rootStore"
 import { PiniaInstances } from "./types/PiniaInstances"
 
-function defineStore(nameOrOptions : any,options : any) {
+function defineStore(nameOrOptions : any,setup : any) {
   if(arguments.length === 0) {
     throw new Error("no arguments, need arguments")
   }
   let name : string
+  let options : any
   if(typeof nameOrOptions === "string") {
     name = nameOrOptions
+    options = setup
   } else {
     name = nameOrOptions.name
     options = nameOrOptions
@@ -36,14 +38,37 @@ function createOptionsStore(name : string,options : any,pinia : PiniaInstances) 
 
   let scpoe
   const store = reactive({})
-  const setup = () => {
-    const localState = pinia.state.value[name] = state ? state() : {}
-    return localState
+  function setup() {
+    pinia.state.value[name] = state ? state() : {}
+    const localState = toRefs(pinia.state.value[name])
+    return Object.assign(
+      localState,
+      Object.keys(getters || {}).reduce((computedGetters : any,name : any) => {
+        computedGetters[name] = computed(() => {
+          return getters[name].call(store,store)
+        })
+        return computedGetters
+      },{}),
+      actions
+    )
   }
   const setupStore = pinia.scpoe.run(() => {
     scpoe = effectScope()
     return scpoe.run(() => setup())
   })
+
+  function wrapAction(name : string,action : Function) {
+    return () => {
+      const res = action.apply(store,arguments)
+      return res
+    }
+  }
+  for(let key in setupStore) {
+    const prop = setupStore[key]
+    if(typeof prop === "function") {
+      setupStore[key] = wrapAction(key,prop)
+    }
+  }
 
   Object.assign(store,setupStore)
   scpoeMap.set(name,store)
